7d6222cf27144aaeec718de4639de4bc4bcb8949
andreyserdyuk-sys <andrey.serdyuk@creativecode.digital>
2026-08-15 23:55:54 +0530

    bug fix

--- supabase/migrations/20250815120000_alter_purchases_price_columns_to_int.sql
+++ supabase/migrations/20250815120000_alter_purchases_price_columns_to_int.sql
@@ -2,3 +2,8 @@
 ALTER TABLE purchases
     ALTER COLUMN unit_price TYPE INTEGER USING ROUND(unit_price),
     ALTER COLUMN total_cost TYPE INTEGER USING ROUND(total_cost);
+
+-- Set default values for new integer columns
+ALTER TABLE purchases
+    ALTER COLUMN unit_price SET DEFAULT 0,
+    ALTER COLUMN total_cost SET DEFAULT 0;
